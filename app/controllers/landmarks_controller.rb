e5df8346b420faefec09dea8277b568cb7f31f18
danny galimi <dannygalimi13@aim.com>
2021-03-12 19:22:38 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -8,11 +8,11 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :"landmarks/new"
   end
-#
-#   get '/landmarks/:id' do
-#     @landmark = Landmark.find(params[:id])
-#     erb :"landmarks/show"
-#   end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :"landmarks/show"
+  end
 #
 #   get '/landmarks/:id/edit' do
 #     @landmark = Landmark.find(params[:id])
